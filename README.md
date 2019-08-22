21d7a8c4be3f841b76722faaff0d75faaf7adc31
Christopher Butcher <christopher.butcher@shopify.com>
2019-08-22 16:12:04 -0400

    asynchronously copy clipboard, preview if changed

--- README.md
+++ README.md
@@ -14,7 +14,8 @@ $ brew install secret-sender
 `secret-sender` requires two users to run the program at the same time in cooperation, and paste messages to each other.
 These messages are not secret so they can be sent over (e.g.) Slack.
 
-1. The user receving the secret token or password will run `secret-sender receive`. This will copy their public key to the clipboard and display it in their shell.
+1. The user receving the secret token or password will run `secret-sender receive`. This will copy their public key to
+   the clipboard and display it in their shell.
 2. The receiving user will message that public key to the sender.
 3. The sender will run `secret-sender send`, paste the public key into their shell, and press Enter/Return.
 4. The sender will then be prompted to copy their secret to their clipboard and press Enter/Return.
@@ -23,12 +24,14 @@ These messages are not secret so they can be sent over (e.g.) Slack.
 7. The receiver copies that encrypted string into their clip and presses Enter/Return in secret-sender.
 8. secret-sender displays the secret in the shell.
 
-
 ## Under the hood
 
 Secret-sender uses NaCl Box cryptograpy, or curve25519xsalsa20poly1305.
-The receiver generates an ephemeral keypair and sends the public portion to the sender, who encrypts the secret to that key, before sending the ciphertext to the receiver. The receiver then recovers the plaintext and terminates, discarding the private key.
+The receiver generates an ephemeral keypair and sends the public portion to the sender, who encrypts the secret to that
+key, before sending the ciphertext to the receiver. The receiver then recovers the plaintext and terminates, discarding
+the private key.
 
-Neither subcommand takes any arguments, but both ask the sender and receiver to copy their plaintext and ciphertext (respectively) into their clipboard once the program is running.
+Neither subcommand takes any arguments, but both ask the sender and receiver to copy their plaintext and ciphertext
+(respectively) into their clipboard once the program is running.
 
 Scripting with secret-sender is discouraged: instead, use [ejson](http://github.com/Shopify/ejson) directly.
--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/Shopify/ejson/crypto"
 	"github.com/atotto/clipboard"
 )
 
+const clipboardPreviewLength = 50
+
 var stdin *bufio.Reader
+var publicKeyBytes []byte
 
 func init() {
 	stdin = bufio.NewReader(os.Stdin)
@@ -31,17 +38,22 @@ func main() {
 	}
 }
 
+func usageAndDie() {
+	fmt.Fprintln(os.Stderr, "usage: secret-sender send|receive")
+	os.Exit(1)
+}
+
 func sendSecret() {
 	fmt.Println("Ask the receiving party to run `secret-sender receive` and send you the public key that it generates.")
 	fmt.Println("Paste the public key here:")
-	pk := readline()
-
-	bytes, err := hex.DecodeString(string(pk))
+	publicKeyBytes = readline()
+	hexPublicKeyBytes, err := hex.DecodeString(string(publicKeyBytes))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	var receiverKP crypto.Keypair
-	for i, b := range bytes {
+	for i, b := range hexPublicKeyBytes {
 		receiverKP.Public[i] = b
 	}
 
@@ -50,9 +62,10 @@ func sendSecret() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Copy your secret to your clipboard, then press Enter/Return:")
-	readEnter()
-	plaintext := pbpaste()
+	fmt.Println("Copy your secret into your clipboard:")
+	plaintextResult := make(chan []byte)
+	go getClipboardContents(plaintextResult, "encrypt it")
+	plaintext := <-plaintextResult
 
 	encrypter := crypto.NewEncrypter(&ephemeralKP, receiverKP.Public)
 	ciphertext, err := encrypter.Encrypt(plaintext)
@@ -60,8 +73,7 @@ func sendSecret() {
 		log.Fatal(err)
 	}
 
-	pbcopy(string(ciphertext))
-
+	copyToClipboard(string(ciphertext))
 	fmt.Println("This is the encrypted string. Paste it to the receiver. (We've already put it in your clipboard):")
 	fmt.Println(string(ciphertext))
 }
@@ -71,31 +83,27 @@ func receiveSecret() {
 	if err := kp.Generate(); err != nil {
 		log.Fatal(err)
 	}
+	publicKey := kp.PublicString()
+	publicKeyBytes = []byte(publicKey)
+	copyToClipboard(publicKey)
 
 	fmt.Println("Paste this key to the sender (we've already put it in your clipboard):")
-	pbcopy(kp.PublicString())
-	fmt.Println(kp.PublicString())
+	fmt.Println(publicKey)
 
-	fmt.Println("They'll respond with a big encrypted-looking blob. Copy it to your clip board, then press Enter/Return:")
-	readEnter()
-	ciphertext := pbpaste()
+	fmt.Println("They'll respond with a big encrypted-looking blob. Once you receive it, copy it to your clipboard:")
+	ciphertextResult := make(chan []byte)
+	go getClipboardContents(ciphertextResult, "decrypt it")
+	ciphertext := <-ciphertextResult
 
 	decrypter := &crypto.Decrypter{Keypair: &kp}
 	plaintext, err := decrypter.Decrypt(ciphertext)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Println("This is the secret:")
 	fmt.Println(string(plaintext))
-}
-
-func usageAndDie() {
-	fmt.Fprintln(os.Stderr, "usage: secret-sender send|receive")
-	os.Exit(1)
-}
-
-func readEnter() {
-	fmt.Scanln()
 }
 
 func readline() []byte {
@@ -106,13 +114,55 @@ func readline() []byte {
 	return line
 }
 
-func pbcopy(text string) {
+func getClipboardContents(textResult chan []byte, action string) {
+	done := make(chan bool)
+
+	go getClipboardContentsInner(done, textResult, action)
+	go waitUntilEnter(done)
+}
+
+func getClipboardContentsInner(done chan bool, textResult chan []byte, action string) {
+	var clipboardContents []byte
+
+	for {
+		select {
+		case <-done:
+			textResult <- clipboardContents
+			return
+		default:
+			newClipboardContents := copyFromClipboard()
+
+			if bytes.Compare(newClipboardContents, clipboardContents) != 0 && bytes.Compare(newClipboardContents, publicKeyBytes) != 0 {
+				newClipboardPreview := strings.Join(strings.Fields(strings.TrimSpace(string(newClipboardContents))), " ")
+				newClipboardPreviewLength := math.Min(float64(len(newClipboardPreview)), clipboardPreviewLength)
+
+				fmt.Printf(
+					"Got new clipboard contents: length=%d preview='%s'. Press enter/return to %s.\n",
+					len(newClipboardContents),
+					newClipboardPreview[0:int(newClipboardPreviewLength)],
+					action,
+				)
+
+				clipboardContents = newClipboardContents
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func waitUntilEnter(done chan bool) {
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	done <- true
+}
+
+func copyToClipboard(text string) {
 	if err := clipboard.WriteAll(text); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func pbpaste() []byte {
+func copyFromClipboard() []byte {
 	text, err := clipboard.ReadAll()
 	if err != nil {
 		log.Fatal(err)
